705dc91ff1a3884cb4e6e17b9c1e7f5c2e26574f
Mya Tavizon <mya.td12@gmail.com>
2025-07-09 14:58:04 -0500

    Added textfield and a button

--- Interavtice Ui/ContentView.swift
+++ Interavtice Ui/ContentView.swift
@@ -12,7 +12,19 @@ struct ContentView: View {
         VStack {
             Text("What is your name")
                 .font(.title)
+            TextField("type name here", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .padding()
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.gray, width: 1)
+            Button("Submit Name") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(Color(hue: 0.903, saturation: 0.309, brightness: 0.982))
         }
+        .padding()
     }
 }
 
