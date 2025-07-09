1a1cf9fac95849645d2221405e2014b81e4123a7
Mya Tavizon <mya.td12@gmail.com>
2025-07-09 14:34:25 -0500

    Initial Commit

---
+++ Interavtice Ui/Interavtice_UiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Interavtice_UiApp.swift
+//  Interavtice Ui
+//
+//  Created by Scholar on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Interavtice_UiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
